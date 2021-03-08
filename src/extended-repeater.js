b894731378333edd617edfc15a92f7a1d7c1c166
Victoria Iurina <Victoria_iurina@epam.com>
2021-03-09 01:37:14 +0300

    feat: add new function for 'Extended repeater'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let repeatTimes = 1;
+    let separator = '+';
+    let addition = '';
+    let additionRepeatTimes = 1;
+    let additionSeparator = '|';
+    let newString = '';
+
+  if(options.separator) {
+    separator = options.separator;
+  }
+  if(options.additionSeparator) {
+    additionSeparator = options.additionSeparator;
+  }
+  if(options.repeatTimes > 1) {
+    repeatTimes = options.repeatTimes;
+  }
+  if(options.additionRepeatTimes > 1) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  if("addition" in options) {
+    addition = options.addition;
+  }
+  for(let i = 0; i < repeatTimes; i++) {
+    newString += str;
+    if(addition !== '') {
+      for(let j = 0; j < additionRepeatTimes; j++) {
+        newString += addition;
+        if( j+1 !== additionRepeatTimes) {
+          newString += additionSeparator;
+        }
+      }
+    }
+    if( i+1 !== repeatTimes) {
+      newString += separator;
+    }
+  }
+  return newString;
+
 };
-  
