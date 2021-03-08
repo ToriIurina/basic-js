d227e2886967620c5105c0a9d6da936d06161f9c
Victoria Iurina <Victoria_iurina@epam.com>
2021-03-09 00:32:16 +0300

    feat: add new function for 'Vigenere cipher'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,80 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('error');
+    }
+    let alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let m = message.toUpperCase();
+    let k = key.toUpperCase();
+    let result = '';
+    let mi;
+    let ki;
+    let j = 0;
+    let i = 0;
+    let c;
+    let maxlength = Math.max(m.length, k.length);
+    while (i < maxlength) {
+      if (i >= m.length) {
+        break;
+      } else {
+        mi = alf.indexOf(m[i]);
+      }
+      if (mi === -1) {
+        result += m[i];
+        i++;
+        continue;
+      }
+      ki = alf.indexOf(k[j >= k.length ? j % k.length : j]);
+      c = alf[(alf.length + (mi + ki)) % 26];
+      result += c;
+      i++;
+      j++;
+    }
+    if (!this.direct) {
+      return result.split('').reverse().join('');
+    }
+    return result;
+  }
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('error');
+    }
+    let alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let m = message.toUpperCase();
+    let k = key.toUpperCase();
+    let result = '';
+    let mi;
+    let ki;
+    let j = 0;
+    let i = 0;
+    let c;
+    let maxlength = Math.max(m.length, k.length);
+    while (i < maxlength) {
+      if (i >= m.length) {
+        break;
+      } else {
+        mi = alf.indexOf(m[i]);
+      }
+      if (mi === -1) {
+        result += m[i];
+        i++;
+        continue;
+      }
+      ki = alf.indexOf(k[j >= k.length ? j % k.length : j]);
+      c = alf[(alf.length + (mi - ki)) % 26];
+      result += c;
+      i++;
+      j++;
+    }
+    if (!this.direct) {
+      return result.split('').reverse().join('');
+    }
+    return result;
   }
 }
 
